e4cf5f77ca56ce897ded930ac9a9f29833eee79c
Reparadoxo <joaofe.mokdse@gmail.com>
2025-11-30 16:10:09 -0300

    Added functions

--- main_calculadora.py
+++ main_calculadora.py
@@ -1,20 +1,25 @@
-try:
-    num1 = float(input("Digite o primeiro número da calculadora:\n"))
-    sinais = input("Digite a operação desejada (+, -, *, /):\n")
-    num2 = float(input("Insira o segundo número da calculadora:\n"))
-    match sinais:
-        case '+':
-            resultado = num1 + num2
-        case '-':
-            resultado = num1 - num2
-        case '/':
-                resultado = num1 / num2
-        case '*':
-            resultado = num1 * num2
-        case _:
-            raise ValueError("O símbolo está fora do escopo.")
-    print(f"O resultado é: {resultado}")
-except ValueError:
-    print("Você deve digitar números")
-except ZeroDivisionError:
-     print("Não se pode dividir por 0.")
+
+num1 = float(input("Digite o primeiro número da calculadora:\n"))
+sinais = input("Digite a operação desejada (+, -, *, /):\n")
+num2 = float(input("Insira o segundo número da calculadora:\n"))
+
+def calculadora(num1, num2, sinais):
+    try:
+        match sinais:
+            case '+':
+                resultado = num1 + num2
+            case '-':
+                resultado = num1 - num2
+            case '/':
+                    resultado = num1 / num2
+            case '*':
+                resultado = num1 * num2
+            case _:
+                print("O símbolo está fora do escopo.")
+        print(f"O resultado é: {resultado}")
+    except ValueError:
+        print("Você deve digitar números")
+    except ZeroDivisionError:
+        print("Não se pode dividir por 0.")
+
+calculadora(num1, num2, sinais)
